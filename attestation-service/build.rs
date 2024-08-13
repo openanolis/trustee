fc2e1609c22fe30369facad0e767272974ff31c5
Jiale Zhang <zhangjiale@linux.alibaba.com>
2024-08-13 15:08:54 +0800

    AS: Dockerfile add GPU Attestation dependencies
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- attestation-service/build.rs
+++ attestation-service/build.rs
@@ -5,6 +5,10 @@ fn real_main() -> Result<(), String> {
     tonic_build::compile_protos("../protos/attestation.proto").map_err(|e| format!("{e}"))?;
 
     tonic_build::compile_protos("../protos/reference.proto").map_err(|e| format!("{e}"))?;
+
+    println!("cargo:rustc-link-lib=python3.11");
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    
     Ok(())
 }
 
--- deps/verifier/build.rs
+++ deps/verifier/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-lib=python3");
+    println!("cargo:rustc-link-lib=python3.11");
     println!("cargo:rustc-link-search=native=/usr/lib");
 }
