35d465e4ed141b36c737e3559f3d33aa6dbd4a83
Jiale Zhang <zhangjiale@linux.alibaba.com>
2025-02-13 15:18:17 +0800

    Use AnolisOS as components image base OS
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- deps/verifier/build.rs
+++ deps/verifier/build.rs
@@ -1,4 +1,22 @@
 fn main() {
-    println!("cargo:rustc-link-lib=python3.12");
+    let python_version = std::env::var("PYTHON_VERSION").unwrap_or_else(|_| "3.12".to_string());
+
+    match python_version.as_str() {
+        "3.8" => {
+            println!("cargo:rustc-link-lib=python3.8");
+        }
+        "3.12" => {
+            println!("cargo:rustc-link-lib=python3.12");
+        }
+        _ => {
+            println!(
+                "cargo:warning=Unsupported Python version: {}",
+                python_version
+            );
+            println!("cargo:warning=Defaulting to Python 3.12");
+            println!("cargo:rustc-link-lib=python3.12");
+        }
+    }
+
     println!("cargo:rustc-link-search=native=/usr/lib");
 }
